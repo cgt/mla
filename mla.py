4dc884c5407e4f19e4704e133f09554f9f57b7de
Christoffer G. Thomsen <chris@cgt.name>
2016-09-26 12:42:44 +0200

    Release to the public domain with CC0

--- mla.py
+++ mla.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
+# mla - mail.log analyzer
+# Written in 2016 by Christoffer G. Thomsen <chris@cgt.name>
+#
+# To the extent possible under law, the author(s) have dedicated
+# all copyright and related and neighboring rights to this software
+# to the public domain worldwide. This software is distributed
+# without any warranty. You should have received a copy of the CC0
+# Public Domain Dedication along with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
+
 import argparse
 import operator
 from pprint import pprint
